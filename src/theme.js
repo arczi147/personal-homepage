073cb32e2929dde784cd5beef0f8e98d51e66691
Artur Feliks <arturfeliks4@gmail.com>
2023-10-24 13:49:28 +0200

    new colors to theme.js added

--- src/theme.js
+++ src/theme.js
@@ -4,5 +4,7 @@ export const theme = {
         whiteLilac: "#FBFBFE",
         slateGray: "#6E7E91",
         scienceBlue: "#0366D6",
+        mineShaft: "#252525",
+        iron: "#D1D5DA",
     },
 };
