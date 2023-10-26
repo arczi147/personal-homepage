b4dc9ade62e11cb18e4b7b954305d6178b0c3841
Artur Feliks <arturfeliks4@gmail.com>
2023-10-26 16:57:37 +0200

    Component 'Content' created; component 'Loading' and 'Error' moved to it

---
+++ src/features/author/PersonalHomepage/Portfolio/Content/Error/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { ReactComponent as DangerIcon } from "../../../../../../images/danger.svg";
+import { ButtonLink } from "../../../../ButtonLink";
+
+export const StyledError = styled.div`
+    text-align: center;
+    margin-top: 88px;
+`;
+
+export const Icon = styled(DangerIcon)`
+    width: 48px;
+    height: 48px;
+`;
+
+export const Message = styled.h2`
+    color: ${({ theme }) => theme.color.mineShaft};
+    font-size: 24px;
+    margin: 18px 0 32px 0;
+`;
+
+export const Info = styled.span`
+    color: ${({ theme }) => theme.color.mineShaft};
+    font-size: 20px;
+    line-height: 1.4;
+    display: block;
+`;
+
+export const StyledButtonLink = styled(ButtonLink)`
+    margin: 16px auto 140px;
+    display: inline-flex;
+`;
---
+++ src/features/author/PersonalHomepage/Portfolio/Content/Loading/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { ReactComponent as LoadingIcon } from "../../../../../../images/spinner.svg";
+
+export const StyledLoading = styled.div`
+    text-align: center;
+    margin-top: 88px;
+`;
+
+export const Info = styled.span`
+    color: ${({ theme }) => theme.color.mineShaft};
+    font-size: 20px;
+    line-height: 1.4;
+    display: block;
+    margin-bottom: 48px;
+`;
+
+export const Icon = styled(LoadingIcon)`
+    width: 160px;
+    height: 160px;
+    margin-bottom: 140px;
+`;
