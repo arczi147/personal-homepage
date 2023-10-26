def7ff550314345fa5826b25a96b7ce9542eb039
Artur Feliks <arturfeliks4@gmail.com>
2023-10-26 16:45:13 +0200

    '@reduxjs/toolkit', 'react-redux' and 'redux-saga' installed

--- src/features/author/PersonalHomepage/Portfolio/Loading/index.js
+++
@@ -1,8 +0,0 @@
-import { Icon, Info, StyledLoading } from "./styled";
-
-export const Loading = () => (
-    <StyledLoading>
-        <Info>Please wait, projects are being loaded...</Info>
-        <Icon />
-    </StyledLoading>
-);
