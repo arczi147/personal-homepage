75b6e26386355bb70151cf30d6551108448ed21b
Artur Feliks <arturfeliks4@gmail.com>
2023-10-26 17:00:32 +0200

    Component 'Portfolio' updated

--- src/features/author/PersonalHomepage/Portfolio/index.js
+++ src/features/author/PersonalHomepage/Portfolio/index.js
@@ -1,11 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Content } from "./Content";
 import { Heading } from "./Heading";
-import { Error } from "./Error";
-import { Loading } from "./Loading";
+import { fetchProjects, selectProjects, selectProjectsStatus } from "./projectsSlice";
+import { useEffect } from "react";
+import { Wrapper } from "./styled";
 
-export const Portfolio = () => (
-    <>
-        <Heading />
-        <Loading />
-        <Error />
-    </>
-);
+export const Portfolio = () => {
+    const dispatch = useDispatch();
+
+    const projects = useSelector(selectProjects);
+    const projectsStatus = useSelector(selectProjectsStatus);
+
+    useEffect(() => {
+        dispatch(fetchProjects());
+    }, [dispatch]);
+
+    return (
+        <Wrapper>
+            <Heading />
+            <Content projects={projects} status={projectsStatus} />
+        </Wrapper>
+    )
+};
---
+++ src/features/author/PersonalHomepage/Portfolio/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 1216px;
+    height: 500px;
+`;
