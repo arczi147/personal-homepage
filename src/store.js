544c8a7fba55cbb3136162974d1ec4bd745f75f5
Artur Feliks <arturfeliks4@gmail.com>
2024-01-09 21:23:05 +0100

    saving theme in localStorage added

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import projectsReducer from "./features/author/PersonalHomepage/Portfolio/projectsSlice";
+import themeSwitchReducer from "./features/author/PersonalHomepage/Header/ThemeSwitcher/themeSwitchSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         projects: projectsReducer,
+        themeSwitch: themeSwitchReducer,
     },
     middleware: [sagaMiddleware],
 });
